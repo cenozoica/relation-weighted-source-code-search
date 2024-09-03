c6314f7ac460d0ce0f9197a051e76807b9029551
cenozoica <vadim.baitoiu@gmail.com>
2024-09-03 11:51:35 +0200

    refactor Python plot script

--- SourceCodeComplexity/plotfilecomplexity.py
+++ SourceCodeComplexity/plotfilecomplexity.py
@@ -1,31 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.rcParams['font.family'] = 'Courier'
 logpath = '/Users/vadimbaitoiu/Downloads/complexitydata.csv'
 complexityMap = np.loadtxt(logpath, comments="#", unpack=False)
 complexityMap = np.transpose(complexityMap)
 complexityMax, fileIndexMax = complexityMap.shape
 x = np.arange(0, fileIndexMax, 1)
-fig, axs = plt.subplots(4, 1, layout='constrained')
-axs[0].plot(x, complexityMap[0, :], color='dodgerblue')
-axs[0].set_xlabel('File')
-axs[0].set_ylabel('Line count')
-axs[0].grid(True)
-axs[1].plot(x, complexityMap[1, :], color='lime')
-axs[1].set_xlabel('File')
-axs[1].set_ylabel('Comment energy')
-axs[1].grid(True)
-axs[2].plot(x, complexityMap[2, :], color='cyan')
-axs[2].set_xlabel('File')
-axs[2].set_ylabel('Relation energy')
-axs[2].grid(True)
-axs[3].plot(x, complexityMap[3, :], color='tomato')
-axs[3].set_xlabel('File')
-axs[3].set_ylabel('Search result energy')
-axs[3].grid(True)
 
-'''
-cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
-axs[1].set_ylabel('Coherence')
-'''
+complexityName = ['Line count', 'Comment energy', 'Relation energy', 'Search result energy']
+complexityColor = ['darkgoldenrod', 'forestgreen', 'deepskyblue', 'orangered']
+fig, axs = plt.subplots(len(complexityName), 1, sharex=True, layout='constrained')
+
+for i in range(len(complexityName)):
+    axs[i].plot(x, complexityMap[i, :], color=complexityColor[i])
+    axs[i].set_title(complexityName[i], fontsize=20, color=complexityColor[i], y=0.55, va='top')
+    axs[i].grid(False)
+    axs[i].axis(False)
+
 plt.show()
